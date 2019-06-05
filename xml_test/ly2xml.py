84d8ce2151496be9e4e1a5cc41feb8d96e40ec13
Bryant <bryantgeorge1@sbcglobal.net>
2019-06-05 09:53:51 -0400

    * ly2xml Tests: Improve readability and output

--- xml_test/ly2xml.py
+++ xml_test/ly2xml.py
@@ -2,19 +2,22 @@
 This file is merely a utility to be used with command:
 python3 -m ly2xml </path/to/input.ly> </path/to/output.musicxml>
 """
-import os, sys
-sys.path.insert(0, os.path.abspath(".."))  # from https://stackoverflow.com/questions/9427037/relative-path-not-working-even-with-init-py
+import os, sys  # Help from https://stackoverflow.com/questions/9427037/relative-path-not-working-even-with-init-py
+sys.path.insert(0, os.path.abspath(".."))
 import ly.musicxml
+
+def ly2xml(inFile, outFile):
+    """Function to read in a lilypond input and write to a musicxml output"""
+    # Read
+    e = ly.musicxml.writer()
+    f = open(inFile, 'r', encoding='utf-8')
+    e.parse_text(f.read(), inFile)
+    f.close()
+    # Write
+    xml = e.musicxml()
+    xml.write(outFile)
+
 if len(sys.argv) != 3:
     print("Command must be in form ly2xml </path/to/input.ly> </path/to/output.musicxml>")
 else:
-    ipath = sys.argv[1]
-    opath = sys.argv[2]
-    # read
-    e = ly.musicxml.writer()
-    f = open(ipath, 'r', encoding='utf-8')
-    e.parse_text(f.read(), ipath)
-    f.close()
-    # write
-    xml = e.musicxml()
-    xml.write(opath)
+    ly2xml(sys.argv[1], sys.argv[2])
--- xml_test/test.py
+++ xml_test/test.py
@@ -1,9 +1,10 @@
 """
 Tests for XML output.
-Taken from /python-ly/tests/test_xml.py as a model
+/python-ly/tests/test_xml.py taken as a model
+Use with command: python3 -m test
 """
-import sys, os, io
-sys.path.insert(0, os.path.abspath(".."))  # from https://stackoverflow.com/questions/9427037/relative-path-not-working-even-with-init-py
+import sys, os, io  # Help from https://stackoverflow.com/questions/9427037/relative-path-not-working-even-with-init-py
+sys.path.insert(0, os.path.abspath(".."))
 import datetime
 import difflib
 import ly.musicxml
@@ -11,9 +12,13 @@ from lxml import etree
 
 def test_all():
     """Test all input files against output files in /test_media"""
-    for i in range(1, 10):
-        input("Press enter to test file {}".format(i))
-        compare_output(str(i))
+    failed_tests = 0
+    for i in range(1, 10):  # Performs 9 tests
+        input("Press enter to test file {}\n".format(i))
+        if not compare_output(str(i)):
+            failed_tests += 1
+    assert failed_tests == 0, "{} test(s) failed\n".format(failed_tests)
+    print("All tests passed\n")  # Prints if no tests failed
 
 def ly_to_xml(filename):
     """Read Lilypond file and return XML string."""
@@ -21,7 +26,9 @@ def ly_to_xml(filename):
     with open(filename, 'r', encoding='utf-8') as lyfile:
         writer.parse_text(lyfile.read())
     xml = writer.musicxml()
-    return ly.musicxml.create_musicxml.xml_decl_txt.format(encoding='UTF-8') + "\n" + ly.musicxml.create_musicxml.doctype_txt + "\n" + xml.tostring(encoding='unicode')
+    return (ly.musicxml.create_musicxml.xml_decl_txt.format(encoding='UTF-8') + "\n"
+        + ly.musicxml.create_musicxml.doctype_txt + "\n"
+        + xml.tostring(encoding='unicode'))
 
 def read_expected_xml(filename):
     """Return string with expected XML from file."""
@@ -39,28 +46,30 @@ def compare_output(filename):
     output = ly_to_xml(filebase + '.ly')
     expected_output = read_expected_xml(filebase + '.musicxml')
 
-    assert_multi_line_equal(expected_output, output, filename)
+    return are_multi_line_equal(expected_output, output, filename)
 
 
-def assert_multi_line_equal(first, second, filename, msg=None):
-    """Assert that two multi-line strings are equal.
-
-    If they aren't, show a nice diff.
+def are_multi_line_equal(first, second, filename, msg=None):
     """
-    assert isinstance(first, str), 'First argument is not a string'
-    assert isinstance(second, str), 'Second argument is not a string'
+    Returns whether or not two multi-line strings are equal.
+    If they are, indicate that the test succeeded for filename
+    If they aren't, indicate that the test failed for filename
+        and show a nice diff.
+    """
+    assert isinstance(first, str), 'First argument is not a string\n'
+    assert isinstance(second, str), 'Second argument is not a string\n'
 
     if first == second:
-        print("Test succeeded for file {}".format(filename))
+        print("Test succeeded for file {}\n".format(filename))
+        return True
     else:
         message = ''.join(difflib.ndiff(first.splitlines(True),
             second.splitlines(True)))
         if msg:
             message += " : " + msg
 
-        print(message)  # print diff
-        print("Test failed for file {}, diff is above".format(filename))
-
-    # assert first==second, "Multi-line strings are unequal:\n" + message  # prints diff
+        print(message)  # Print diff
+        print("Test failed for file {}, diff is above\n".format(filename))
+        return False
 
 test_all()
